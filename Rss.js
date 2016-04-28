7fed430f97996052aea456cb0f6fd4332798110e
Daniel-Sand <ds2546a@student.american.edu>
2016-04-28 17:27:20 -0400

    Update Rss.js

--- Rss.js
+++ Rss.js
@@ -1,15 +1,14 @@
-(function () {
-    "use strict";
-    
+
     window.onload = function () {
         sidebar();
-        feeds();
-    }; 
+        //feeds();
+    };
     
     function sidebar(){
         var sidebar = document.getElementById("nav");
         for(var i = 0; i<7 ; i++){
             var newEle = document.createElement("BUTTON");
+            newEle.className = "navbtn";
             var t = document.createTextNode("Category");  
             newEle.appendChild(t);
             sidebar.appendChild(newEle);
@@ -26,4 +25,25 @@
             feeds.appendChild(newEle);
         } 
     }
-})();
+    
+function showRSS() {
+  var ele = document.getElementById("search");
+  var str = ele.value;
+  if (str.length==0) { 
+    document.getElementById("rssOutput").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("GET","getrss.php?q="+str,true);
+  xmlhttp.send();
+}
