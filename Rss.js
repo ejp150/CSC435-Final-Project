ec5347e021ab9f5eca1f6ca341cac56edb9304d6
Daniel-Sand <ds2546a@student.american.edu>
2016-05-01 19:15:43 -0400

    Update Rss.js

--- Rss.js
+++ Rss.js
@@ -1,49 +1,41 @@
 
-    window.onload = function () {
-        sidebar();
-        //feeds();
-    };
-    
-    function sidebar(){
-        var sidebar = document.getElementById("nav");
-        for(var i = 0; i<7 ; i++){
-            var newEle = document.createElement("BUTTON");
-            newEle.className = "navbtn";
-            var t = document.createTextNode("Category");  
-            newEle.appendChild(t);
-            sidebar.appendChild(newEle);
-        }
-    }
-    
-    function feeds(){
-        var feeds = document.getElementById("feeds");
-        for(var i = 0; i<20 ; i++){
-            var newEle = document.createElement("div");
-            newEle.className = "feed";
-            var t = document.createTextNode("content");  
-            newEle.appendChild(t);
-            feeds.appendChild(newEle);
-        } 
-    }
-    
-function showRSS() {
-  var ele = document.getElementById("search");
+//get search term
+function searchRSS(){
+var ele = document.getElementById("search");
   var str = ele.value;
-  if (str.length==0) { 
+    RSS(str);
+}
+
+function clicked(str){
+    RSS(str);
+}
+
+//get RSS
+function RSS(str) {
+  if (str.length ===0) { 
     document.getElementById("rssOutput").innerHTML="";
     return;
   }
   if (window.XMLHttpRequest) {
     // code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp=new XMLHttpRequest();
-  } else {  // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
+    
+  //when ready out put
   xmlhttp.onreadystatechange=function() {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
-    }
-  }
+  document.getElementById("feeds").innerHTML=xmlhttp.responseText;
+  };
   xmlhttp.open("GET","getrss.php?q="+str,true);
   xmlhttp.send();
 }
+
+function link(str){
+   var t = window.Event.target;
+t.location.href = str;
+}
+window.onload = function () {
+        //sidebar();
+        //feeds()
+        RSS(" ");
+    };
+
